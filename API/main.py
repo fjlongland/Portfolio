b55f1f9bd9573039a22ba5e683e99ebb03ac0ba0
fjlongland <fjlongland@gmail.com>
2024-06-22 17:42:59 +0200

    added comments 1

--- API/main.py
+++ API/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return{"message:" "Hi There trveler!"}
+@app.get("/") #decorator references app(instance of fats API you are using) and specifys file path to the changes you are making
+async def root(): #init the function and specify its name
+    return{"message:" "Hi There trveler!"} #code that runs in the function and makes changes at specified destiantion
+
+@app.get("/post")
+def get_posts():
+    return{"data": "This is your post!"}
