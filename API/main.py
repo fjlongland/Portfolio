d9047920cfc3aa127a4e6b25e271fb17937f688b
fjlongland <fjlongland@gmail.com>
2024-06-24 11:22:44 +0200

    added get singularpost functionality

--- API/main.py
+++ API/main.py
@@ -19,6 +19,12 @@ class Post(BaseModel): #post class to handle validation of posts
 my_posts =[{"title": "Title of post 1", "content": "Content of post 1", "published": True, "rating": 1, "id": 11111}, 
            {"title": "Title of post 2", "content": "Content of post 2", "published": False, "rating": None, "id": 11112}]
 
+
+def findPost(id):  # simple for loop to find post with ID: id 
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/") #decorator references app(instance of fats API you are using) and specifys file path to the changes you are making
 async def root(): #init the function and specify its name
     return{"message:" "Hi There trveler!"} #code that runs in the function and makes changes at specified destiantion
@@ -34,6 +40,15 @@ def create_post(post: Post):
     print(post_dict["id"])
     my_posts.append(post_dict)
     return {"data": post_dict}#return the whole post to display on postman
+
+
+
+@app.get("/post/{id}")
+def get_post(id: int):  #you can validage the input like this to make shure an int has been input(wors for all data types)
+    print(id)
+    wpost = findPost(id)  #calls function to find post with ID: id
+    print(wpost)  
+    return{"post": wpost}
 
 
 #TODO : Create data base to actually store posts
