30fc8f91f141fc8cea6be6d2df67abb07f5c559f
fjlongland <fjlongland@gmail.com>
2024-07-07 14:48:18 +0200

    fixed some issues with hashing function

---
+++ API/APP/utils.py
@@ -0,0 +1,6 @@
+from passlib.context import CryptContext #hashing library to hash paswords before storage
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")#specifying hashing algorythm for password storage
+
+def hash(password: str):
+    return pwd_context.hash(password)
