75652a4ac89631daa55c08fb51191c899de3d93e
fjlongland <fjlongland@gmail.com>
2024-07-18 15:10:48 +0200

    small change to token generation

--- API/APP/routers/auth.py
+++ API/APP/routers/auth.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import schemas, models, utils, oauth2 
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=['Autentication'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 
     user = db.query(models.User).filter(models.User.username == user_credentials.username).first()
 
