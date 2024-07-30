abf0df071a1e44c115207baefa2f23a084751c00
fjlongland <fjlongland@gmail.com>
2024-07-30 20:53:51 +0200

    adder owner details when returning a post

--- API/APP/models.py
+++ API/APP/models.py
@@ -2,6 +2,7 @@ from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 
 class Post(Base):
     __tablename__="posts"
@@ -12,6 +13,9 @@ class Post(Base):
     published = Column(Boolean, nullable=False, server_default='TRUE')
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     user_id_fk = Column(Integer, ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+
+    owner = relationship("User")
+
 
 class User(Base):
     __tablename__="users"
--- API/APP/schemas.py
+++ API/APP/schemas.py
@@ -11,20 +11,6 @@ class PostBase(BaseModel): #post class to handle validation of posts
 class PostCreate(PostBase):
     pass
 
-class Post(PostBase): #this class defines the response model for the postst that are returned in the appication
-    id: int
-    created_at: datetime
-    user_id_fk: int
-
-
-    class Config:
-        orm_model = True
-
-class UserCreate(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-
 class User(BaseModel): #response model for users
     username: str
     email: str
@@ -33,6 +19,19 @@ class User(BaseModel): #response model for users
     class Config:
         orm_model = True
 
+class Post(PostBase): #this class defines the response model for the postst that are returned in the appication
+    id: int
+    created_at: datetime
+    user_id_fk: int
+    owner: User
+
+    class Config:
+        orm_model = True
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
 
 class UserLogin(BaseModel):
     username: str
